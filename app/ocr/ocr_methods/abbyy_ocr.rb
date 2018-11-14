1ae926ba992e690913749c2557e9e020bbbff8b6
M C McGrath <shidash@shidash.com>
2018-11-14 04:25:34 -0500

    Updated abbyy ocr handling of errors

--- app/ocr/ocr_methods/abbyy_ocr.rb
+++ app/ocr/ocr_methods/abbyy_ocr.rb
@@ -2,6 +2,13 @@
 module AbbyyOCR
   # OCR the file
   def ocr_with_abbyy(file)
-    return %x[abbyyocr11 -c -if #{"'"+file+"'"} -f TextVersion10Defaults -tel -tet UTF8 -tcp Latin].gsub(/\xef\xbb\xbf/, "")
+    text = %x[abbyyocr11 -c -if #{"'"+file+"'"} -f TextVersion10Defaults -tel -tet UTF8 -tcp Latin].gsub(/\xef\xbb\xbf/, "")
+
+    # Only return text from ABBYY if it exited successfully
+    if $?.to_s.split("exit ").last == "0"
+      return text
+    else
+      throw Error
+    end
   end
 end
